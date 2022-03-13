a49eb7295c6d881859cd0dc02543b9b3b2bf34f5
Amy Keibler <amelia.keibler@gmail.com>
2022-03-12 21:15:08 -0500

    Continue implementing functionality
    - Add dependabot
    - Add tests for modified JSON
    - Improve error handling and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use eyre::bail;
+use crate::config::{ConfigurationFromFile, ConfigurationSource};
+use eyre::Context;
 
 mod config;
 mod templates;
 
 fn main() -> color_eyre::Result<()> {
+    tracing_subscriber::fmt::init();
     color_eyre::install()?;
-    let config = config::load_configuration()?;
-    println!("Got configuration: {config:?}");
-
-    if config.backup_folder.exists() && config.backup_folder.is_dir() {
-        println!("Backup folder set up correctly");
-    } else {
-        bail!(
-            "Backup folder does not exist: {}",
-            config.backup_folder.to_string_lossy()
-        );
-    }
-
-    println!("Hello, Amy!");
+    let config = ConfigurationFromFile::load_configuration()
+        .wrap_err("Could not load configuration from the application's configuration folder")?;
 
     Ok(())
 }
